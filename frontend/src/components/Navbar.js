0834cc2f23e29f723ed292d36f99849cbf349a3c
matias <you@example.com>
2024-01-05 01:54:39 -0300

    added link functionality to navbar items

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,22 +1,31 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
 
 
 function Navbar() {
+
+
+  const navigate = useNavigate();
+
+  const redirectToCart = () => {
+    navigate('/Cart');
+  };
+
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Electronic Components</a>
+            <Link to="/" class="navbar-brand">Electronic Components</Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto align-items-start"> 
                     <li class="nav-item">
-                        <a class="nav-link mt-1 mt-md-0" aria-disabled="true">
+                        < div class="nav-link mt-1 mt-md-0" aria-disabled="true" onClick={redirectToCart}>
                             Cart
                             <span class="badge bg-primary rounded-pill m-1">3</span>
-                        </a>
+                        </div>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" aria-expanded="false">Search Products</a>
