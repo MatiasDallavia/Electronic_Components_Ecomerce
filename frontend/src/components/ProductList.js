28dec6a6f9215c8f94af0017925703781bdc4399
matias <you@example.com>
2024-01-04 23:23:02 -0300

    created Paginator

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,91 +1,36 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table';
 import ProductTableRow from './ProductTableRow';
-
+import Paginatior from './Paginatior';
 
 function ProductList() {
     
   return (
 
-    <div class="container mt-5 mb-5">
-    <Table bordered hover product-table>
-      <thead>
-        <tr>
-            <th scope="col">Product</th>
-            <th scope="col">Description</th>
-            <th scope="col">Price</th>
-            <th scope="col">Units Available</th>
-            <th scope="col">Package</th>
-            <th scope="col">Manufacturer</th>
-            <th scope="col">In Cart</th>
-        </tr>
-      </thead>
-      <tbody>
-        <ProductTableRow/>
-        <ProductTableRow/>
-        <ProductTableRow/>
-        <ProductTableRow/>        
-      </tbody>
-    </Table>
+    <div className="container d-flex flex-column align-items-center">
+        <Table bordered hover product-table id="products-table">
+        <thead>
+            <tr>
+                <th scope="col">Product</th>
+                <th scope="col">Description</th>
+                <th scope="col">Price</th>
+                <th scope="col">Units Available</th>
+                <th scope="col">Package</th>
+                <th scope="col">Manufacturer</th>
+                <th scope="col">In Cart</th>
+            </tr>
+        </thead>
+        <tbody>
+            <ProductTableRow/>
+            <ProductTableRow/>
+            <ProductTableRow/>
+            <ProductTableRow/>        
+        </tbody>
+        </Table>
 
 
-      <div class="d-flex flex-column align-items-center">
+        <Paginatior/>
 
-        <nav>
-                <ul class="pagination justify-content-end">
-                <li class="page-item disabled">
-                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-                </li>
-                <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">...</a></li>
-                <li class="page-item"><a class="page-link" href="#">4</a></li>
-                <li class="page-item"><a class="page-link" href="#">5</a></li>
-                <li class="page-item"><a class="page-link" href="#">6</a></li>
-
-                <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
-                </li>
-                </ul>
-            </nav>
-            <nav>
-                <ul class="pagination justify-content-end">
-                <li class="page-item">
-                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">...</a></li>
-                <li class="page-item active"><a class="page-link" href="#">5</a></li>
-                <li class="page-item"><a class="page-link" href="#">...</a></li>
-                <li class="page-item"><a class="page-link" href="#">10</a></li>
-                <li class="page-item"><a class="page-link" href="#">12</a></li>
-
-                <li class="page-item">
-                    <a class="page-link" href="#">Next</a>
-                </li>
-                </ul>
-            </nav>
-            <nav>
-                <ul class="pagination justify-content-end">
-                <li class="page-item">
-                    <a class="page-link" href="#" tabindex="-1">Previous</a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item"><a class="page-link" href="#">...</a></li>
-                <li class="page-item"><a class="page-link" href="#">4</a></li>
-                <li class="page-item"><a class="page-link" href="#">5</a></li>
-                <li class="page-item active"><a class="page-link" href="#">6</a></li>
-
-                <li class="page-item disabled">
-                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Next</a>
-                </li>
-                </ul>
-            </nav>
-        </div>
     </div>    
   )
 }
