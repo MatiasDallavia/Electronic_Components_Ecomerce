9f58029f2783018d3b61d0ea9696d489fcf18971
matias <you@example.com>
2024-01-09 01:13:16 -0300

    Created TransistorInput and completed the rest of the inputs

--- backend/products/schema/inputs.py
+++ backend/products/schema/inputs.py
@@ -1,18 +1,37 @@
 import graphene
-from products.enums import BJTTypesEnum
+from products.schema.enums import (
+    TransistorTypesEnum,
+    MOSFETPackagesOptionsEnum,
+    BJTPackagesOptionsEnum,
+    IGBTPackagesOptionsEnum,
+    BJTTypesEnum,
+    ManufacturerEnum
+
+)
 
 class BJTInput(graphene.InputObjectType):
+    manufacturer = graphene.Field(ManufacturerEnum)
     bjt_type = graphene.Field(BJTTypesEnum)
+    ic_max = graphene.Float()
+    vce_saturation = graphene.Float()
+    dc_current_gain = graphene.Int()
 
 
 class MOSFETInput(graphene.InputObjectType):
-    transitor_type = graphene.String()
-    bjt_type = graphene.Field(BJTTypesEnum)
+    manufacturer = graphene.Field(ManufacturerEnum)
+    vds = graphene.Float()
+    drive_voltage = graphene.Float()
+    rds_on = graphene.Float()
 
 
 class IGBTInput(graphene.InputObjectType):
-    transitor_type = graphene.String()
-    bjt_type = graphene.Field(BJTTypesEnum)
+    manufacturer = graphene.Field(ManufacturerEnum)
+    vc = graphene.Float()
+    ic = graphene.Float()
+    power_max = graphene.Float()
 
-class TransistorInput(BJTInput):
-    transistorType = graphene.String()
+class TransistorInput(graphene.InputObjectType):
+    transistor_type = graphene.Field(TransistorTypesEnum)
+    bjt_input= graphene.Field(BJTInput)
+    mosfet_input = graphene.Field(MOSFETInput)
+    igbt_input = graphene.Field(IGBTInput)
