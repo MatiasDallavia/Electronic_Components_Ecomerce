455e4610cff4b3c4d63ffba8cb5bbf799889c09a
matias <you@example.com>
2024-01-10 01:26:50 -0300

    Moved queries from Query to ProductListQuery

--- backend/products/schema/schema.py
+++ backend/products/schema/schema.py
@@ -4,6 +4,9 @@ from products.schema.types import BJTType, MOSFETType
 
 from ..models import BJT, MOSFET
 from products.schema.enums import BJTPackagesOptionsEnum, ManufacturerEnum, MountingTechnologyEnum
-from products.schema.queries import Query
+from products.schema.list_product_queries import ProductListQuery
+
+class Query(ProductListQuery):
+    pass
 
 schema = graphene.Schema(query=Query)
