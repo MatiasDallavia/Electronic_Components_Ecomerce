e7e5904163506b8240fa2d664f411402181fc865
matias <you@example.com>
2024-01-11 20:34:29 -0300

    created  dir graphql_queries

---
+++ frontend/src/graphql_queries/InductorListQuery.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+const GET_INDUCTORS = gql`
+query GetInductors($inputs: InductorInput!){
+  inductorListQuery(inputs: $inputs) {
+      id
+      model
+      description
+      price
+      mountingTechnology
+      amountAvailable
+      manufacturer
+      package    
+  }
+}`;
+
+
+const InductorInput = {
+  inputs: {
+    model: null,
+    mountingTechnology: null,
+    manufacturer: null,
+    inductorType: null,
+    coreMaterial: null,
+    inductance: null,
+    current: null 
+  }
+}
+
+
+export {GET_INDUCTORS, InductorInput}
+
+
