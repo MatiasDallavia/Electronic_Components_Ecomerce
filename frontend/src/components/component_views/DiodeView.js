61b1802dfce05d7c95419be9a0dfbb806d5faabf
matias <you@example.com>
2024-01-15 15:18:54 -0300

    Refactored DiodeView to query based on input ID parameter and updated css style

--- frontend/src/components/component_views/DiodeView.js
+++ frontend/src/components/component_views/DiodeView.js
@@ -1,21 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { GET_SINGLE_DIODE, singleDiodeInput } from '../../graphql_queries/single_product_query/SingleDiodeQuery';
 import ProductCharacteristicRow from './ProductCharacteristicRow';
 
+import inductor330 from '../../images/components/A_330.jpg'
+
+import { parseComponentAttributeText } from '../../uitls';
+
+
 
 
 function DiodeView() {
+ 
+  const ToggleInCart = (e) => {
+        console.log("FUNC")
+        const button = e.target
+        console.log(button.innerHTML)
+        if (button.className === "btn btn-success btn-lg btn-block"){
+            button.className = "btn btn-warning btn-lg btn-block"
+            button.innerHTML = "Remove from Cart"
+        } else{
+            button.className = "btn btn-success btn-lg btn-block"
+            button.innerHTML = "Add to Cart"
+        }
+  }
 
-  const { componentModel } = useParams();
-  console.log("params: ",componentModel)
-  singleDiodeInput.inputs.id = 10 
-  const { loading, error, data } = useQuery(GET_SINGLE_DIODE, {singleDiodeInput});
-  console.log(data)
-  const diode = data ? data.diodeListQuery : [];
-  console.log(diode)
-  console.log(error)
+  const { diodeComponentID } = useParams();
+  console.log(diodeComponentID)
+  singleDiodeInput.inputs.id = diodeComponentID 
+  const { loading, error, data } = useQuery(GET_SINGLE_DIODE, {variables: singleDiodeInput});
+
+  const excludedFields = new Set(['__typename', 'componentType', 'model', 'price', 'amountAvailable']);
+
+  const diode = data ? data.diodeListQuery[0] : [];
+
+  const diodeAttributes = Object.keys(diode).map((key) => {
+    if (!excludedFields.has(key)) {
+        return [key, diode[key]];
+    }
+    return null;
+    }).filter(Boolean);
 
   return (
     <div class="container pt-5">
@@ -25,34 +51,34 @@ function DiodeView() {
             
                 <div class="container d-flex flex-column align-items-center justify-content-between main-attributes">
                         <div>
-                        <img id="cover-image-product" src="..."/>
+                        <img id="cover-image-product" src={inductor330}/>
 
                         <table>
                             <tr>
-                            <td>Model</td>
-                            <th>1N706A</th>
+                            <td class="ModelCell">Model</td>
+                            <th class="ModelCell">{diode.model}</th>
                             </tr>
                             <tr>
                             <td>Price</td>
-                            <td>$100</td>
-                            </tr>
-                            <tr>
-                            <td>Product Type</td>
-                            <td>Zener Diode</td>
+                            <td>${diode.price}</td>
                             </tr>
                             <tr>
                             <td>Units Available</td>
-                            <td>$300</td>
+                            <td>{diode.amountAvailable}</td>
                             </tr>
                             <tr>
-                            <td>Brown</td>
-                            <td>$250</td>
+                            <td>Component Type</td>
+                            <td>Diode</td>
                             </tr>
                         </table>
                     </div>
                     <div>
                         <label class=" d-flex flex-column align-items-center">Added to cart !</label>
-                        <button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
+                        <button 
+                            type="button" 
+                            class="btn btn-success btn-lg btn-block"
+                            onClick={(e) => ToggleInCart(e)}
+                        >Block level button</button>
                     </div>
                 </div>
         </div>
@@ -64,7 +90,14 @@ function DiodeView() {
                 </tr>
                 </thead>
                 <tbody>
-                    <ProductCharacteristicRow attribute={23} value={23}/>
+                    {
+                        diodeAttributes.map((attr) => (
+                            <ProductCharacteristicRow 
+                                attribute={parseComponentAttributeText(attr[0])} 
+                                value={attr[1]}
+                            />
+                        ))
+                    }
                 </tbody>
             </table>
             </div>
