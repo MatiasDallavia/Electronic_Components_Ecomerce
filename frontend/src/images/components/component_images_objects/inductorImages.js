7d1dd5c58a903dd8de1a7ec94023c5b83c70312a
matias <you@example.com>
2024-01-16 23:50:37 -0300

    Fixed Images

--- frontend/src/images/components/component_images_objects/inductorImages.js
+++ frontend/src/images/components/component_images_objects/inductorImages.js
@@ -1,13 +1,15 @@
 import A330 from "../A_330.jpg"
 import A220 from "../A_220.jpeg"
 import A101 from "../A_101.webp"
+import P2220 from "../P2220.jpeg"
 import TOROID from "../TOROID_DIL.png"
 
 
 const inductorImages = {
-  A_330: A330,
-  A_220: A220,
-  A_101: A101,
+  P330: A330,
+  P220: A220,
+  P101: A101,
+  P2220: P2220,
   DIL: TOROID,
 };
 
