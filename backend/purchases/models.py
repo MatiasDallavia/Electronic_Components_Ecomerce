bf97ff9209224e871035166a0d604e8141b692b4
matias <you@example.com>
2024-01-19 18:51:42 -0300

    Created and migrated ProductPurchase

--- backend/purchases/models.py
+++ backend/purchases/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ProductPurchase(models.Model):
+    COMPONENT_TYPES = {
+        "BJT": "BJT",
+        "MOSFET": "MOSFET",
+        "IGBT": "IGBT",
+        "CAPACITOR": "Capacitor",
+        "RESISTOR": "Resistor",
+        "DIODE": "Diode",
+        "INDUCTOR": "Inductor",
+
+    }
+    component_type = models.CharField(choices=COMPONENT_TYPES, default="BJT", null=False)
+    component_id = models.IntegerField(null=False)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=False)
+    price = models.FloatField(null=False)
+    quantity = models.IntegerField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def total_price(self):
+        return self.quantity * self.price
+    
+        
