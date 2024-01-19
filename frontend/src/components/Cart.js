6c241aa476bef7c81ad9d91a4ee6b1fb46c13709
matias <you@example.com>
2024-01-19 14:34:46 -0300

    refactored Cart component

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react';
 import CartItemTableRow from './CartItemTableRow';
 
 import {removeFromCart} from "../utils/cartFunctions"
@@ -6,6 +7,7 @@ import {removeFromCart} from "../utils/cartFunctions"
 function Cart() {
 
   const productsInCart = JSON.parse(localStorage.getItem('cart')) || [];
+  const [totalValue, serTotalValue] = useState(0)
 
   const removeItemFromList = (componentType, componentID) => {
     document.querySelector(`#${componentType + componentID}`).remove()
@@ -20,20 +22,28 @@ function Cart() {
         </div>
 
         <div className="row">
-
           <div className="col-md-5 col-lg-6 order-md-last">
             <h4 className="d-flex justify-content-between align-items-center mb-3">
               <span className="text-primary">List of Products</span>
-              <span className="badge bg-primary rounded-pill">{productsInCart.length}</span>
+              <span className="badge bg-primary rounded-pill">
+                {productsInCart.length}
+              </span>
             </h4>
             <ul className="list-group mb-3">
-
-                {productsInCart.map((product) => <CartItemTableRow removeItemFromList={removeItemFromList} componentType={product[0]} componentID={product[1]}/> )}
-
-                <li className="list-group-item d-flex justify-content-between">
-                  <span>Total (USD)</span>
-                  <strong>$20</strong>
-                </li>
+              {productsInCart.map((product) => (
+                <CartItemTableRow
+                  key={`${product[0]}-${product[1]}`}
+                  removeItemFromList={removeItemFromList}
+                  componentType={product[0]}
+                  componentID={product[1]}
+                  serTotalValue={serTotalValue}
+                  totalValue={totalValue}
+                />
+              ))}
+              <li className="list-group-item d-flex justify-content-between">
+                <span>Total (USD)</span>
+                <strong>${totalValue}</strong>
+              </li>
             </ul>
           </div>
 
@@ -41,44 +51,58 @@ function Cart() {
             <h4 className="mb-3">Billing address</h4>
             <form className="needs-validation" noValidate>
               <div className="row g-3 pb-4">
-                <div className='col-4'>
+                <div className="col-4">
                   <label htmlFor="address" className="form-label">
                     Address
                   </label>
-                  <input type="text" className="form-control" id="address" placeholder="1234 Main St" required />
-              </div>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="address"
+                    placeholder="1234 Main St"
+                    required
+                  />
+                </div>
               </div>
 
               <div className="row g-3 pb-4">
-   
                 <div className="col-md-3">
-                  <label for="country" class="form-label">Country</label>
-                    <select class="form-select" id="country" required>
-                      <option value="">Choose...</option>
-                      <option>United States</option>
-                      <option>Argentina</option>
-                    </select>
-                  <div class="invalid-feedback">
+                  <label htmlFor="country" className="form-label">
+                    Country
+                  </label>
+                  <select className="form-select" id="country" required>
+                    <option value="">Choose...</option>
+                    <option>United States</option>
+                    <option>Argentina</option>
+                  </select>
+                  <div className="invalid-feedback">
                     Please select a valid country.
                   </div>
                 </div>
-                
-                <div className="col-md-3">
 
-                    <label for="state" class="form-label">State</label>
-                    <select class="form-select" id="state" required>
+                <div className="col-md-3">
+                  <label htmlFor="state" className="form-label">
+                    State
+                  </label>
+                  <select className="form-select" id="state" required>
                     <option value="">Choose...</option>
                     <option>California</option>
-                    </select>
-                    <div class="invalid-feedback">
+                  </select>
+                  <div className="invalid-feedback">
                     Please provide a valid state.
-                    </div>
+                  </div>
                 </div>
                 <div className="col-md-3">
                   <label htmlFor="zip" className="form-label">
                     Zip
                   </label>
-                  <input type="text" className="form-control" id="zip" placeholder="" required />
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="zip"
+                    placeholder=""
+                    required
+                  />
                   <div className="invalid-feedback">Zip code required.</div>
                 </div>
               </div>
