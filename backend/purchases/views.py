8bda870e78122d8a0910a34190f0d6908ee0fdf5
matias <you@example.com>
2024-01-19 21:42:01 -0300

    Created create-order endpoint

--- backend/purchases/views.py
+++ backend/purchases/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def create_order(request):
+    return JsonResponse({"STATUS":"SUCCSESFUL"})
