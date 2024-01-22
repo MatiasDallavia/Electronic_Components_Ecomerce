bdc4462e890c9e8ae277f02437711d6f0f47a961
matias <you@example.com>
2024-01-22 17:59:25 -0300

    Added query to Register.js an Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,19 +1,67 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
+import { useMutation } from '@apollo/client';
+import {LOGIN_USER} from "../graphql_queries/user_query/LoginMutation"
+
+import {saveTokens} from "../utils/token"
 
 function Login() {
+
+  const [mutationVariables, setMutationVariables] = useState({
+    username: null,
+    password: null
+  });
+
+  const [loginMutation, { loading, error, data }] = useMutation(LOGIN_USER, {variables : mutationVariables});
+  
+
+
+  const handleInputFormChange = (formField, value) => {
+    
+    setMutationVariables({
+      ...mutationVariables,
+      [formField]: value
+    });
+    console.log(mutationVariables)
+  };  
+
+  const handleLogin = async () => {
+    console.log("first")
+    try {
+      const result = await loginMutation({
+        variables: mutationVariables
+      });
+      console.log(result.data); // Handle success
+      saveTokens(result.data.login)
+    } catch (error) {
+      console.error(error); // Handle error
+    }
+  };  
+
   return (
-    <div class="d-flex justify-content-center align-items-center vh-100">
-        <div class="container" style={{width: 500, height: 700}}>
+    <div class="d-flex justify-content-center align-items-center vh-100 m-5">
+        <div class="container m-5" style={{width: 500, height: 700}}>
         <form>
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            <h1 class="h3 mb-3 fw-normal">Please Log In</h1>
     
             <div class="form-floating">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"/>
-            <label for="floatingInput">Email address</label>
+            <input 
+              type="username" 
+              class="form-control" 
+              id="floatingInput" 
+              placeholder="name@example.com"
+              onChange={(e)=>handleInputFormChange("username", e.target.value)}  
+            />
+            <label for="floatingInput">Username</label>
             </div>
             <div class="form-floating">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
+            <input 
+              type="password" 
+              class="form-control" 
+              id="floatingPassword" 
+              placeholder="Password"
+              onChange={(e)=>handleInputFormChange("password", e.target.value)}                
+            />
             <label for="floatingPassword">Password</label>
             </div>
     
@@ -21,7 +69,13 @@ function Login() {
             </div>
             <Link to="/register">Not registred yet?</Link>
             <hr/>
-            <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+            <button 
+              class="btn btn-primary w-100 py-2" 
+              type="button"
+              onClick={handleLogin}
+            >
+              Log in
+            </button>
         </form>
         </div>
     </div>
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,35 +1,91 @@
 import React from 'react'
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
+import { useMutation } from '@apollo/client';
+import {REGISTER_USER} from "../graphql_queries/user_query/RegisterMutation"
+import { useNavigate } from "react-router-dom";
+
 
 function Register() {
+
+  const navigate = useNavigate();
+
+  const [mutationVariables, setMutationVariables] = useState({
+    username: null,
+    email: null,
+    password: null
+  });
+
+  const [registerUser, { loading, error, data }] = useMutation(REGISTER_USER, {variables : mutationVariables});
+  
+
+  const handleInputFormChange = (formField, value) => {
+    
+    setMutationVariables({
+      ...mutationVariables,
+      [formField]: value
+    });
+    console.log(mutationVariables)
+  };  
+
+  const handleRegister = async () => {
+    console.log("first")
+    try {
+      const result = await registerUser({
+        variables: mutationVariables
+      });
+      console.log(result.data); // Handle success
+      navigate("/login")      
+    } catch (error) {
+      console.error(error); // Handle error
+    }
+  };  
+
   return (
-    <div class="d-flex justify-content-center align-items-center vh-100">
-        <div class="container" style={{width: 500, height: 700}}>
+    <div class="d-flex justify-content-center align-items-center vh-100 mt-5">
+        <div class="container mt-5" style={{width: 500, height: 700}}>
         <form>
-            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+            <h1 class="h3 mb-3 fw-normal">Please Sign In</h1>
     
             <div class="form-floating">
-            <input type="email" class="form-control" placeholder="name@example.com"/>
+            <input 
+              type="email" 
+              class="form-control" 
+              placeholder="name@example.com" 
+              onChange={(e) => handleInputFormChange("email", e.target.value)}/>
             <label for="floatingInput">Email address</label>
             </div>
             <div class="form-floating">
-                <input type="username" class="form-control" placeholder="Username"/>
+                <input 
+                  type="username" 
+                  class="form-control" 
+                  placeholder="Username"
+                  onChange={(e) => handleInputFormChange("username", e.target.value)}                
+                />
                 <label for="floatingPassword">Username</label>
             </div>
             <div class="form-floating">
-            <input type="password" class="form-control" placeholder="Password"/>
+            <input 
+              type="password" 
+              class="form-control" 
+              placeholder="Password"
+              onChange={(e) => handleInputFormChange("password", e.target.value)}
+            />
             <label for="floatingPassword">Password</label>
             </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" placeholder="Password"/>
-                <label for="floatingPassword">Password</label>
-            </div>
+
     
             <div class="form-check text-start my-3">
             </div>
             <Link to="/login">Already registered?</Link>
             <hr/>
-            <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+            <button 
+              class="btn btn-primary w-100 py-2" 
+              onClick={handleRegister} 
+              type="button"
+            >
+              Sign in
+            </button>
         </form>
         </div>
     </div>
