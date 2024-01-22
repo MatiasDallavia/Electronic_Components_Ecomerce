308189d05e55d154c9fca7191fb21bad117f3316
matias <you@example.com>
2024-01-22 14:03:05 -0300

    Added useLocation to PurchaseConfirmation.js

--- frontend/src/components/purchased_products/PurchaseConfirmation.js
+++ frontend/src/components/purchased_products/PurchaseConfirmation.js
@@ -3,10 +3,15 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import PurchasedComponentTable from './PurchasedComponentTable';
 import WaitingSpinner from './WaitingSpinner';
+import { useLocation } from 'react-router-dom';
+
 
 
 function PurchaseConfirmation() {
-  const { token } = useParams();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const token = searchParams.get('token');    
+
   console.log(token)
   let loading = true 
   return (
