c82f74c0a05a0090bbd8d94387d97b48530ec4e6
matias <you@example.com>
2024-01-27 21:29:20 -0300

    Refactored search inductors

--- frontend/src/components/search_components/SearchInductor.js
+++ frontend/src/components/search_components/SearchInductor.js
@@ -1,46 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { useLazyQuery } from '@apollo/client';
 import ProductList from '../ProductList';
 import { GET_LIST_INDUCTORS, InductorListInput } from '../../graphql_queries/list_product_query/InductorListQuery';
 import InductorFilter from './product_filters/InductorFilter';
+import ErrorMessage from '../ErrorMessage';
+
+import {fetchData} from "../../utils/fetchData"
 
 
 
 function SearchInductor() {
 
 
-  const [queryProducts, { loading, error, data }] = useLazyQuery(GET_LIST_INDUCTORS);
   const [queryVariables, setQueryVariables] = useState(InductorListInput);
   const [inductors, setInductors] = useState([]);
-
+  const [errorMesage, setErrorMessage] = useState("An error ocurred");
 
 
   useEffect(() => {
+    getInductors();
+  }, []); 
 
-    // Realizar la consulta al cargar la página
-    queryProducts({ variables: queryVariables });
-  }, []);
+    const getInductors = async () => {
+      try {
+        console.log(queryVariables)
+        const data = await fetchData(GET_LIST_INDUCTORS, queryVariables, setErrorMessage);
+        setInductors(data.inductorsQuery);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setErrorMessage("An error occurred");
+        console.log(errorMesage)
+      }
+    };
+  
 
-  useEffect(() => {
-    if (data) {
-      setInductors(data.inductorsQuery);
-      console.log(inductors)
-    }
-    
-  }, [data]);
-
-
-  const handleSearch = () => {
-    queryProducts({ variables: queryVariables });
-  };
 
   return (
     <div className="container filters g-3">
       <InductorFilter queryVariables={queryVariables} setQueryVariables={setQueryVariables}/>
-      <button type="button" className="btn btn-primary submit" onClick={handleSearch}>
+      <button 
+        type="button" 
+        className="btn btn-primary submit" 
+        onClick={()=>getInductors(GET_LIST_INDUCTORS,queryVariables)}
+      >
         Search
       </button>
-      <ProductList products={inductors} />
+      {errorMesage ?  <ProductList products={inductors} /> : <ErrorMessage error={errorMesage}/> }
     </div>
   )
 }
---
+++ frontend/src/utils/fetchData.js
@@ -0,0 +1,37 @@
+
+async function fetchData(SCHEMA, variables, JWT=null) {
+
+    const HOST = process.env.REACT_APP_HOST;
+    const PORT = process.env.REACT_APP_DJANGO_PORT;
+  
+    const apiUrl = `${HOST}:${PORT}/graphql`
+  
+  
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': JWT,
+        },
+        body: JSON.stringify({
+          query: SCHEMA,
+          variables: variables,
+        }),
+      });
+  
+      if (!response.ok) {
+        throw new Error('An error occurred while doing the query');
+      }
+  
+      const data = await response.json();
+      return data.data
+  
+    } catch (error) {
+      console.error('There was a problem with the query operation:', error);
+  
+    }
+  }
+
+
+export {fetchData}  
