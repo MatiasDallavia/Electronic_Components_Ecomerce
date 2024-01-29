9b7eec67dc7b15c1b5332179b7339f521a2237f0
matias <you@example.com>
2024-01-29 16:00:36 -0300

    updated mosfet filter

--- frontend/src/components/search_components/product_filters/MOSFETFilter.js
+++ frontend/src/components/search_components/product_filters/MOSFETFilter.js
@@ -4,35 +4,34 @@ import { handleNestedFieldChange } from '../../../utils/callbacks';
 function MOSFETFilter({setQueryVariables}) {
   return (
     <>
+        <div class="filter-group me-3">
+            <label for="currentCollector" class="filter-label">Voltage Drain Source(Vds):</label>
+            <select 
+                class="form-select filter-field type transistor"
+                onChange={handleNestedFieldChange(setQueryVariables, 'mosfetInput', 'vds')}
+            >
+                <option value="ALL">All</option>
+                <option value="8">8</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
+                <option value="30">30</option>
+            </select>
+        </div>    
+
 
         <div class="filter-group me-3">
-            <label for="vds" class="filter-label">Voltage Drain Source(Vds):</label>
-            <input 
-              class="form-control filter-field" 
-              type="text" 
-              placeholder="Default input"
-              onChange={handleNestedFieldChange(setQueryVariables, 'mosfetInput','vds')}
-            />
-        </div>
-
-        <div class="filter-group me-3">
-            <label for="rdsOn" class="filter-label">Rds ON:</label>
-            <input 
-              class="form-control filter-field" 
-              type="text" placeholder="Default input"
-              onChange={handleNestedFieldChange(setQueryVariables, 'mosfetInput','rdsOn')}
-            />
-        </div>
-
-        <div class="filter-group me-3">
-            <label for="drive_voltage" class="filter-label">Drive Voltage:</label>
-            <input 
-              class="form-control filter-field" 
-              type="text" 
-              placeholder="Default input"
-              onChange={handleNestedFieldChange(setQueryVariables, 'mosfetInput','driveVoltage')}
-            />
-        </div>
+            <label for="rdsON" class="filter-label">Rds ON:</label>
+            <select 
+                class="form-select filter-field type transistor"
+                onChange={handleNestedFieldChange(setQueryVariables, 'mosfetInput', 'rdsOn')}
+            >
+                <option value="ALL">All</option>
+                <option value="0.08">0.08</option>
+                <option value="0.05">0.05</option>
+                <option value="0.1">0.1</option>
+            </select>
+        </div>    
+          
   
     </>
   )
