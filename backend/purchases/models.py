9eaf69a49c2ff63d07b1066e9f3bcc6d39756487
matias <you@example.com>
2024-03-06 17:04:19 -0300

    added properties

--- backend/purchases/models.py
+++ backend/purchases/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from products.models import BJT, MOSFET, IGBT, Capacitor, Resistor, Diode, Inductor
 class ProductPurchase(models.Model):
     COMPONENT_TYPES = {
         "BJT": "BJT",
@@ -12,6 +12,16 @@ class ProductPurchase(models.Model):
         "INDUCTOR": "Inductor",
 
     }
+    COMPONENT_MODELS = {
+        "BJT": BJT,
+        "MOSFET": MOSFET,
+        "IGBT": IGBT,
+        "Capacitor": Capacitor,
+        "Resistor" : Resistor,
+        "Diode" : Diode,
+        "Inductor" : Inductor,
+    }
+    
     component_type = models.CharField(choices=COMPONENT_TYPES, default="BJT", null=False)
     component_id = models.IntegerField(null=False)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=False)
@@ -22,5 +32,13 @@ class ProductPurchase(models.Model):
     @property
     def total_price(self):
         return self.quantity * self.price
+
+    @property
+    def get_component_class(self):
+        return self.COMPONENT_MODELS[self.component_type]
+    
+    def get_component_object(self):
+        component_class = self.get_component_class()
+        return component_class.objects.filter(id=self.component_id).first()
     
         
